4ee89fb4a0356ec65f3383002b9d33560d727db9
qeed97 <qeed97@gmail.com>
2024-12-18 20:47:50 +0100

    created allyContainer component

---
+++ Frontend/src/components/AllyContainer.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import Healthbar from './Healthbar.jsx';
+
+export default function AllyContainer({ allyPokemon, gameData }) {
+    return (
+        <div className="ally-container">
+            <div className="ally-picture">
+                <img src={allyPokemon.sprites.back_default} alt={allyPokemon.name}/>
+            </div>
+            <div className="ally-data">
+                <p>{allyPokemon.name}</p>
+                <Healthbar currentHealth={gameData.ally.hp} health={allyPokemon.stats[0].base_stat}/>
+            </div>
+        </div>
+    );
+};
