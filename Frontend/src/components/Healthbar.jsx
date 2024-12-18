545ae02963e2da4104a91963b737a1ea7c0dfc2d
qeed97 <qeed97@gmail.com>
2024-12-18 20:59:40 +0100

    created Healthbar component

---
+++ Frontend/src/components/Healthbar.jsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+
+export default function Healthbar({healt, currentHealt}) {
+    return (
+        <div className="healthbar-container">
+            <div className="hp-bar"></div>
+            <span className="healthbar-text">{healt}/{currentHealt === undefined ? healt : currentHealt}</span>
+        </div>
+    );
+};
