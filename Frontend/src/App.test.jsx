ded20b99d36798ba6fd02d5748b9b4ecfa65c58d
qeed97 <qeed97@gmail.com>
2025-01-16 17:42:25 +0100

    Loacations page tested

--- Frontend/src/App.test.jsx
+++ Frontend/src/App.test.jsx
@@ -1,6 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { BrowserRouter } from 'react-router-dom';
 import { describe, it, expect } from 'vitest';
 import App from './App';
 
